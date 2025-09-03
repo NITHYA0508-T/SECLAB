36ecb9d410c513b09ec0c84fa939f2d20a917f46
NITHYA0508-T <nithyashreet37@gmail.com>
2025-09-03 09:46:04 +0530

    Create domconcept.js

---
+++ domconcept.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="byId"></div>
+    <div class="container"></div>
+    <div class="container"></div>
+    <script>
+       // document.getElementById("byId").innertext="hai getElementByID";
+        //document.getElementById("byId").style.color="red";
+        //let Res=document.getElementByClassName("container");
+        //Res[0].innerHTML="Hi First container"
+       // Res[0].style.color="red"
+        //document.getElementByIdClassName("container")[1].innerText="second conrtainer";
+    </script>
+</body>
+</html>
